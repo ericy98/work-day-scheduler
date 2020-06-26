942ff69d7de7c618437bef8d848eb0e9e930dfa4
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-06-26 14:17:53 -0500

    add color to changing time

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,31 @@
 var today = moment().format("[Today is] dddd, MMMM Do YYYY");
     $("#currentDay").text(today);
+
+//time selectors
+var timeBlock = $(".time-block");
+var taskText = $("#task-description");
+var saveBtn = $(".saveBtn");
+var hour = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
+
+var timeUpdate = function() {
+    var currentTime = moment().hour();
+    
+    $(".time-block").each(function() {
+        var timeBlock = parseInt($(this).attr("id").split("-")[1])
+        
+        if(timeBlock < currentTime) {
+            $(this).addClass("past");
+        }
+        else if(timeBlock === currentTime) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        }
+        else{
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    });
+
+
+timeUpdate();
